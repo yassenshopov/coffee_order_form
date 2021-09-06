874e2d98096e4aac6a227dfa18ef57e6c3180944
Yassen Shopov <83444316+yassenshopov@users.noreply.github.com>
2021-09-06 09:46:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+document.getElementById("forwardButton").disabled = true;
+
+function leftButtonWidthChange() {
+
+    var winWidth = window.innerWidth;
+    leftButtonWidth = (winWidth - 80)/2;
+    leftButtonWidth = leftButtonWidth + 'px';
+    document.getElementById('buttonWrapper').style.left = leftButtonWidth;
+    console.log(leftButtonWidth)
+}
+
 
 function alphaNumeric(inputString, icon) {
     let input = document.getElementById(inputString)
@@ -29,12 +40,20 @@ function proceedButton() {
     checkFirst = document.getElementById('firstIcon');
     checkLast = document.getElementById('lastIcon');
     if ((checkFirst.style.backgroundImage == 'url("greenTick.png")') && (checkLast.style.backgroundImage == 'url("greenTick.png")')) {
-        document.getElementById('forwardButton').style.backgroundColor = '#3aeb34';
+        document.getElementById('forwardButton').style.backgroundColor = '#a7ed87';
         document.getElementById('forwardButton').style.border = '5px solid #f1f1f1';
         document.getElementById('forwardButton').style.backgroundImage = 'url("arrowRight.png")';
+        document.getElementById('buttonWrapper').style.borderColor = '#a7ed87';
+        document.getElementById("forwardButton").disabled = false;
     }
     else {
         document.getElementById('forwardButton').style.backgroundImage = 'url("threeDots.png")';
         document.getElementById('forwardButton').style.backgroundColor = '#f1f1ff';
+        document.getElementById('buttonWrapper').style.borderColor = '#f1f1f1'; 
     }
+}
+
+function moveForward1() {
+    document.getElementById('fieldset1').style.display = 'none';
+    document.getElementById('fieldset2').style.display = 'block';
 }
